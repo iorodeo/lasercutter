7f3c24b91407ec68ce75bae117326c9c568f867b
Will Dickson <will@iorodeo.com>
2014-11-28 20:08:43 -0800

    Added cnc routines for cutting the middle_left section of the middle floor.

---
+++ floors/middle/cnc/middle_left/laser_plate_pocket.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 150.0
+fileName = 'middle_left_in.dxf'
+depth = 0.125
+startZ = 0.0
+safeZ = 0.5
+overlap = 0.5
+overlapFinish = 0.6
+maxCutDepth = 0.15
+toolDiam =  3/4.0 
+cornerCut = True 
+direction = 'ccw'
+startDwell = 1.0
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = {
+        'fileName'       : fileName,
+        'layers'         : ['LASER_PLATE_POCKET'],
+        'components'     : True,
+        'depth'          : depth,
+        'startZ'         : startZ,
+        'safeZ'          : safeZ,
+        'overlap'        : overlap,
+        'overlapFinish'  : overlapFinish,
+        'maxCutDepth'    : maxCutDepth,
+        'toolDiam'       : toolDiam,
+        'cornerCut'      : cornerCut,
+        'direction'      : direction,
+        'startDwell'     : startDwell,
+        }
+pocket = cnc_dxf.DxfRectPocketFromExtent(param)
+prog.add(pocket)
+
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
