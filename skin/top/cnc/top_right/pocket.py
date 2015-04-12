d66766480107244a2b070cef8237ddf75db848bc
Will Dickson <will@iorodeo.com>
2015-04-11 18:40:25 -0700

    Added gcode generation for top right skin panel.

---
+++ skin/top/cnc/top_right/pocket.py
@@ -0,0 +1,49 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 130.0
+fileName = 'top_right_in.dxf'
+depth = 0.12
+startZ = 0.0
+safeZ = 0.5
+overlap = 0.5
+overlapFinish = 0.6
+maxCutDepth = 0.1
+toolDiam =  3/16.0 
+cornerCut = True 
+direction = 'ccw'
+startDwell = 1.0
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = {
+        'fileName'       : fileName,
+        'layers'         : ['ALIGNMENT_POCKET'],
+        'components'     : True,
+        'depth'          : depth,
+        'startZ'         : startZ,
+        'safeZ'          : safeZ,
+        'overlap'        : overlap,
+        'overlapFinish'  : overlapFinish,
+        'maxCutDepth'    : maxCutDepth,
+        'toolDiam'       : toolDiam,
+        'cornerCut'      : cornerCut,
+        'direction'      : direction,
+        'startDwell'     : startDwell,
+        }
+pocket = cnc_dxf.DxfRectPocketFromExtent(param)
+prog.add(pocket)
+
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
