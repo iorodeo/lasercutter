bb1a442fa829458acd81856e37fbb63603b6ef25
Will Dickson <will@iorodeo.com>
2015-04-23 19:38:14 -0700

    Added gcode generation for laser current meter mount.

---
+++ meter_mount/cnc/m6_hole.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 150.0
+fileName = 'meter_mount.dxf'
+depth = 0.28
+startZ = 0.0
+safeZ = 0.5
+overlap = 0.5
+overlapFinish = 0.5
+maxCutDepth = 0.10
+toolDiam =  1/8.0 
+direction = 'ccw'
+startDwell = 1.0
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = {
+        'fileName'       : fileName,
+        'layers'         : ['M6_THROUGH_HOLE'],
+        'components'     : True,
+        'depth'          : depth,
+        'startZ'         : startZ,
+        'safeZ'          : safeZ,
+        'overlap'        : overlap,
+        'overlapFinish'  : overlapFinish,
+        'maxCutDepth'    : maxCutDepth,
+        'toolDiam'       : toolDiam,
+        'direction'      : direction,
+        'startDwell'     : startDwell,
+        }
+pocket = cnc_dxf.DxfCircPocket(param)
+prog.add(pocket)
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
